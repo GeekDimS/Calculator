527aa52f5ada28d21ae57b2f70c679899011b7f9
DimS <dimsed13@yandex.ru>
2022-06-11 17:34:48 +0300

    Слиты две ветки разных разработчиков

--- controller.py
+++ controller.py
@@ -6,13 +6,13 @@ import logger
 import operations
 import user_interface
 def run():
+    error = False
     #user_interface.Функция, отображающая запрос на ввод данных()
     input_data = data_provider.data_input() #Функция передачи чисел и операций, введённых пользователем
     #user_interface.Функция, отображающая введённую пользователем инфо(input_data)
-    logger.F(f'{input_data[0][0]} + i{input_data[0][1]} {input_data[2]} {input_data[1][0]} + i{input_data[1][1]}')Функция, которая записывает в лог введённые пользователем данные(input_data)
-
+    logger.operations_logger(error, f'{input_data[0][0]} + i{input_data[0][1]} {input_data[2]} {input_data[1][0]} + i{input_data[1][1]}', 'Ввод данных')#Функция, которая записывает в лог введённые пользователем данные(input_data)
     res_operation = operations.operation(input_data[0][0], input_data[0][1], input_data[1][0],input_data[1][1],input_data[2])
-    logger.Функция, которая записывает в лог результат вычислений(res_operation)
+    logger.operations_logger(error) #Функция, которая записывает в лог результат вычислений(res_operation)
     user_interface.Функция, отображающая реультат вычислений и отображающая вопрос, закончить-ли работу калькулятора(res_operation)
     end = data_provider.Функция, возвращающая ответ, закончить-ли работу калькулятора()
     return not end
--- logger.py
+++ logger.py
@@ -5,5 +5,5 @@ def operations_logger(error, result_string, result):
         string_to_file = "Error: " + result_string + '\n' if error else result_string + str(result) +'\n'
         file.write(string_to_file)
 
-operations_logger(False, 'a + b = ', -5)        
+# operations_logger(False, 'a + b = ', -5)        
     
